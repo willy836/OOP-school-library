a514300d8aa131ae796e3bab9f950c1b191df2ad
willy836 <wilsonkimiri836@gmail.com>
2023-03-09 11:41:30 +0300

    Refactor app.rb by moving user list of options logic to main.rb

--- app.rb
+++ app.rb
@@ -16,46 +16,9 @@ class App
     @rentals = []
   end
 
-  def run
-    loop do
-      puts 'Welcome to School Library App!'
-      sleep 0.5
-      puts
-      puts 'Please choose an option by entering a number:'
-      puts '1 - List all books'
-      puts '2 - List all people'
-      puts '3 - Create a person'
-      puts '4 - Create a book'
-      puts '5 - Create a rental'
-      puts '6 - List all rentals for a given person id'
-      puts '7 - Exit'
-      puts
-
-      option = gets.chomp.to_i
-      user_option option
-    end
-  end
-
-  def user_option(input)
-    case input
-    when 1
-      list_all_books
-    when 2
-      list_all_people
-    when 3
-      create_a_person
-    when 4
-      create_a_book
-    when 5
-      create_a_rental
-    when 6
-      list_rentals
-    when 7
-      puts 'Thank you for using this app!'
-      exit
-    else
-      puts 'Invalid option, please enter a number between 1 and 7'
-    end
+  def start_library
+    run
+    user_option
   end
 
   def list_all_books
